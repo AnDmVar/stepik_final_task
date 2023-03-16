250a7671bec5b7ab5881dc065f498f6ee7f045dd
Andmvar <andmvar@gmail.com>
2023-03-17 01:10:31 +0400

    Task: content independence, looking for a bug

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRICE_IN_BASKET = (By.CSS_SELECTOR, ".alertinner p strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_NAME_IN_BASKET = (By.CSS_SELECTOR, ".alert:nth-child(1) .alertinner strong")
+    ADD_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
---
+++ pages/product_page.py
@@ -0,0 +1,29 @@
+from .locators import ProductPageLocators
+from .base_page import BasePage
+
+
+class ProductPage(BasePage):
+    def add_product_in_basket(self):
+        self.should_be_price()
+        self.should_be_button_add_basket()
+        self.browser.find_element(*ProductPageLocators.ADD_BASKET).click()
+
+    def check_product_in_basket(self):
+        self.should_be_right_final_price()
+        self.should_be_right_final_name()
+
+    def should_be_price(self):
+        assert self.is_element_present(*ProductPageLocators.PRICE), "Price is not presented"
+
+    def should_be_button_add_basket(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_BASKET), "Basket is not presented"
+
+    def should_be_right_final_price(self):
+        assert self.is_element_present(*ProductPageLocators.PRICE_IN_BASKET), "The final price is not presented"
+        assert (self.browser.find_element(*ProductPageLocators.PRICE_IN_BASKET).text ==
+                self.browser.find_element(*ProductPageLocators.PRICE).text), "The final price is incorrect"
+
+    def should_be_right_final_name(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME_IN_BASKET), "The final name is not presented"
+        assert (self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_BASKET).text ==
+                self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text), "The final name product is incorrect"
